78540f809128784fd4de0ba91d7c801b20cf2b96
Anand Avati <avati@gluster.com>
2011-07-31 21:12:12 +0530

    quine.c: add /* comment */ support

--- quine.c
+++ quine.c
@@ -1,5 +1,7 @@
+/* what's in a comment, anyways? */
 #include <stdio.h>
 #define Q(str) #str
 #define NL putchar(10)
-void func (char *fmt) { printf (Q(#include <stdio.h>)); NL; printf (Q(#define Q(str) #str)); NL; printf (Q(#define NL putchar(10))); NL; printf (Q(%s), fmt); NL; printf (Q(int main() { func (Q(%s)); }), fmt); NL;}
-int main() { func (Q(void func (char *fmt) { printf (Q(#include <stdio.h>)); NL; printf (Q(#define Q(str) #str)); NL; printf (Q(#define NL putchar(10))); NL; printf (Q(%s), fmt); NL; printf (Q(int main() { func (Q(%s)); }), fmt); NL;})); }
+void printc(char *str) { while (*str) { if (*str != 34) putchar(*str); str++; } }
+void func (char *fmt) { printc (Q("/* what's in a comment, anyways? */")); NL; printf (Q(#include <stdio.h>)); NL; printf (Q(#define Q(str) #str)); NL; printf (Q(#define NL putchar(10))); NL; printf (Q(void printc(char *str) { while (*str) { if (*str != 34) putchar(*str); str++; } })); NL; printf (Q(%s), fmt); NL; printf (Q(int main() { func (Q(%s)); }), fmt); NL;}
+int main() { func (Q(void func (char *fmt) { printc (Q("/* what's in a comment, anyways? */")); NL; printf (Q(#include <stdio.h>)); NL; printf (Q(#define Q(str) #str)); NL; printf (Q(#define NL putchar(10))); NL; printf (Q(void printc(char *str) { while (*str) { if (*str != 34) putchar(*str); str++; } })); NL; printf (Q(%s), fmt); NL; printf (Q(int main() { func (Q(%s)); }), fmt); NL;})); }
